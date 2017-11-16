3deb17281638f5a8412b02e9a252e70c76d93366
giang2u <andre.giang8@etu.univ-lorraine.fr>
2017-11-16 13:37:20 +0100

    suppression mauvais projet

--- archev2/accueil.cpp
+++ archev2/accueil.cpp
@@ -6,28 +6,34 @@
 Accueil::Accueil()
 {
     //On donne une taille par défaut à la fenêtre
-    resize(1000, 600);
+    resize(700, 600);
     setWindowTitle("Accueil");
 
     //On définit les layouts
     //verticalLayout = new QVBoxLayout(this);
     //horizontalLayout = new QHBoxLayout();
-    gridLayout = new QGridLayout(3,3);
+    gridLayout = new QGridLayout();
+    gridLayout->setVerticalSpacing(0);
 
     label = new QLabel(this);
     label->setText("Accueil Arche du turfu");
     label->setAlignment(Qt::AlignHCenter);
-    gridLayout->addWidget(label,1,1);
+    gridLayout->addWidget(label,0,0,0,4);
 
 
     connexion = new QPushButton("Connexion");
-    connexion->setMinimumWidth(8);
-    connexion->setMinimumHeight(6);
-    //gridLayout->addWidget(connexion);
+    connexion->setMaximumWidth(80);
+    connexion->setMaximumHeight(30);
+    gridLayout->addWidget(connexion,0,5);
+
+    label2 = new QLabel(this);
+    label2->setText("Footer");
+    gridLayout->addWidget(label2,5,5);
+
      //horizontalLayout->addWidget(connexion,3,1);
 
 
-    setLayout(horizontalLayout);
+    setLayout(gridLayout);
 
 
 }
